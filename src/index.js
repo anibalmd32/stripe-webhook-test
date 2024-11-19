44000865e97d27448220c41f2b851fd29d5c35e6
Anibal Mendoza <anibalmd32@gmail.com>
2024-11-18 21:46:53 -0400

    firts commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  console.log('Recibiendo evento de stripe');
+  res.send('Hello World!');
+});
+
+app.listen(3001, () => {
+  console.log('Example app listening on port 3000!');
+});
